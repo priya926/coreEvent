6795facc3b743d4ef89d3653561ba63fc2d79275
Sumegha <psumegha6@gmail.com>
2024-06-19 15:16:27 +0530

    now visible index.html file + static enabled
    now visible index.html file + static enabled

--- coreEvent/settings.py
+++ coreEvent/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Event',
+    'Admin'
 ]
 
 MIDDLEWARE = [
@@ -50,11 +53,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'coreEvent.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,10 +114,21 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL ='/static/'
+
+
+STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles ')
+
+STATICFILES_DIR = { 
+     os.path.join (BASE_DIR , "static")
+}
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- coreEvent/urls.py
+++ coreEvent/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Event.views import *
+from Admin.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',homePage,name="homePage"),
 ]
